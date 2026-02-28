59cc09fb9c4f526d904d200156af86315e9099f5
Pedro Lucas <pedrolucasfonseca98@gmail.com>
2026-02-28 03:47:38 -0300

    Refactored to apply OOP concepts

--- Program.cs
+++ Program.cs
@@ -1,84 +1,140 @@
 using System;
 
-class TemperatureConversor
+enum Scale
 {
+    Kelvin,
+    Celsius,
+    Fahrenheit
+}
+
+class Temperature
+{
+    private double value;
+    private Scale scale;
+
+    public Temperature(double value, Scale scale)
+    {
+        this.value = value;
+        this.scale = scale;
+    }
+
+    public double ToKelvin()
+    {
+        switch (scale)
+        {
+            case Scale.Celsius:
+                return value + 273.15;
+            case Scale.Fahrenheit:
+                return (value - 32) * 9/5  + 273.15;
+            default:
+                return 0;
+        }
+    }
+    public double ToCelsius()
+    {
+        switch(scale)
+        {
+            case Scale.Kelvin:
+                return value - 273.15;
+            case Scale.Fahrenheit:
+                return (value - 32) * 5/9;
+            default:
+                return 0;
+        }
+    }
+    public double ToFahrenheit()
+    {
+        switch(scale)
+        {
+            case Scale.Kelvin:
+                return (value - 273.15) * 9/5 + 32;
+            case Scale.Celsius:
+                return (value * 9/5) + 32;
+            default:
+                return 0;
+        }
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Temperature Conversor");
-        Console.WriteLine("1 - Celsius to Fahrenheit");
-        Console.WriteLine("2 - Fahrenheit to Celsius");
+        Console.WriteLine("1 - Celsius to Kelvin");
+        Console.WriteLine("2 - Fahrenheit to Kelvin");
         Console.WriteLine("3 - Kelvin to Celsius");
-        Console.WriteLine("4 - Celsius to Kelvin");
+        Console.WriteLine("4 - Fahrenheit to Celsius");
         Console.WriteLine("5 - Kelvin to Fahrenheit");
-        Console.WriteLine("6 - Fahrenheit to Kelvin");
+        Console.WriteLine("6 - Celsius to Fahrenheit");
         Console.WriteLine("7 - Quit");
         Console.Write("Option: ");
-        string  option = Console.ReadLine() ?? "7";
+        string option = Console.ReadLine() ?? "7";
 
-        double celsius, fahrenheit, kelvin;
+        double kelvin, celsius, fahrenheit;
 
         switch(option)
         {
             case "1":
-                do {
-                        Console.Write("Type the temperature in Celsius: ");
-                        celsius = double.Parse(Console.ReadLine() ?? "0");
-
-                        if (celsius < -273.15)
-                            Console.WriteLine("Celsius cannot be below -273.15. Try again.");
-                        else 
-                        {
-                            fahrenheit = (celsius * 1.8) + 32;
-                            Console.WriteLine($"Result: {fahrenheit:F2}°F");
-                        }
-
-                    } while (celsius < -273.15);
-                break;
-            case "2":
-                do {
-                        Console.Write("Type the temperature in Fahrenheit: ");
-                        fahrenheit = double.Parse(Console.ReadLine() ?? "0");
-
-                        if (fahrenheit < -458.67)
-                            Console.WriteLine("Fahrenheit cannot be below -458.67, Try again");
-                        else 
-                        {
-                            celsius = (fahrenheit - 32) * 5/9;
-                            Console.WriteLine($"Result: {celsius:F2}°C");
-                        }
-
-                    } while (fahrenheit < -458.67);
-                break;
-            case "3":
-                do {
-                        Console.Write("Type the temperature in Kelvin: ");
-                        kelvin = double.Parse(Console.ReadLine() ?? "0");
-
-                        if (kelvin < 0)
-                            Console.WriteLine("Kelvin cannot be below 0. Try again.");
-                        else
-                        {
-                        celsius = kelvin - 273.15;
-                        Console.WriteLine($"Result: {celsius:F2}°C");
-                        }
-
-                    } while(kelvin < 0);
-                break;
-            case "4":
-                do
+                do 
                 {
                     Console.Write("Type the temperature in Celsius: ");
                     celsius = double.Parse(Console.ReadLine() ?? "0");
 
                     if (celsius < -273.15)
-                        Console.WriteLine("Celsius cannot be below -273.15. Try Again");
-                    else
+                        Console.WriteLine("Kelvin cannot be below -273.15. Try again.");
+                    else 
                     {
-                        kelvin = celsius + 273.15;
-                        Console.WriteLine($"Result: {kelvin:F2}°K");
+                        Temperature t = new Temperature(celsius, Scale.Celsius);
+                        Console.WriteLine($"Result: {t.ToKelvin():F2}°K");
                     }
 
                 } while (celsius < -273.15);
+                break;
+            case "2":
+                do 
+                {
+                    Console.Write("Type the temperature in : ");
+                    fahrenheit = double.Parse(Console.ReadLine() ?? "0");
+
+                    if (fahrenheit < -458.67)
+                        Console.WriteLine("Fahrenheit cannot be below -458.67, Try again");
+                    else 
+                    {
+                        Temperature t = new Temperature(fahrenheit, Scale.Fahrenheit);
+                        Console.WriteLine($"Result: {t.ToKelvin():F2}°K");
+                    }
+
+                } while (fahrenheit < -458.67);
+                break;
+            case "3":
+                do 
+                {
+                    Console.Write("Type the temperature in Kelvin: ");
+                    kelvin = double.Parse(Console.ReadLine() ?? "0");
+
+                    if (kelvin < 0)
+                        Console.WriteLine("Kelvin cannot be below 0. Try again.");
+                    else
+                    {
+                        Temperature t = new Temperature(kelvin, Scale.Kelvin);
+                        Console.WriteLine($"Result: {t.ToCelsius():F2}°C");
+                    }
+
+                } while(kelvin < 0);
+                break;
+            case "4":
+                do
+                {
+                    Console.Write("Type the temperature in Fahrenheit: ");
+                    fahrenheit = double.Parse(Console.ReadLine() ?? "0");
+
+                    if (fahrenheit < -458.67)
+                        Console.WriteLine("Fahrenheit cannot be below -458.67. Try Again");
+                    else
+                    {
+                        Temperature t = new Temperature(fahrenheit, Scale.Fahrenheit);
+                        Console.WriteLine($"Result: {t.ToCelsius():F2}°C");
+                    }
+
+                } while (fahrenheit < -458.67);
                 break;
             case "5":
                 do
@@ -90,8 +146,8 @@ class TemperatureConversor
                         Console.WriteLine("Kelvin cannot be below 0. Try again.");
                     else
                     {
-                        fahrenheit = (kelvin - 273.15) * 9/5 + 32;
-                        Console.WriteLine($"Result: {fahrenheit:F2}°F");
+                        Temperature t = new Temperature(kelvin, Scale.Kelvin);
+                        Console.WriteLine($"Result: {t.ToFahrenheit():F2}°F");
                     }
 
                 } while (kelvin < 0);
@@ -99,17 +155,17 @@ class TemperatureConversor
             case "6":
                 do
                 {
-                    Console.Write("Type the temperature in fahrenheit: ");
-                    fahrenheit = double.Parse(Console.ReadLine() ?? "0");
+                    Console.Write("Type the temperature in Celsius: ");
+                    celsius = double.Parse(Console.ReadLine() ?? "0");
 
-                    if (fahrenheit < -458.67)
-                        Console.WriteLine("Fahrenheit cannot be below -458.67. Try Again");
+                    if (celsius < -273.15)
+                        Console.WriteLine("Fahrenheit cannot be below -273.15. Try Again");
                     else
                     {
-                        kelvin = (fahrenheit - 32) * 5/9 + 273.15;
-                        Console.WriteLine($"Result: {kelvin:F2}°K");
+                        Temperature t = new Temperature(celsius, Scale.Celsius);
+                        Console.WriteLine($"Result: {t.ToFahrenheit():F2}°F");
                     }
-                } while (fahrenheit < -458.67);
+                } while (celsius < -273.15);
                 break;
             case "7":
                 Console.WriteLine("Bye!");
